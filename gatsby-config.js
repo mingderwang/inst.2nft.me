e22d955c433dc6829ff0a05d9c74ab2893afc0b9
Ming-der Wang <mingderwang@gmail.com>
2022-03-15 15:06:09 +0800

    test coler-picker in mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout.js"),
+        },
         extensions: [`.mdx`, `.md`],
       },
     },
