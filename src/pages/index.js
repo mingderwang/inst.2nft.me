0d103ef55efd819e43576fa60777636b40befb6b
Ming-der Wang <mingderwang@gmail.com>
2022-03-15 12:48:00 +0800

    init for inst.2nft.me home page

---
+++ src/pages/index.js
@@ -0,0 +1,47 @@
+import { graphql, Link as GatsbyLink } from "gatsby";
+import React from "react";
+import { Box, Heading, Link } from "theme-ui";
+import Layout from "../components/layout";
+
+export default function IndexPage({ data }) {
+  return (
+    <>
+      <Layout>
+        {data.allMdx &&
+          data.allMdx.nodes.map(({ id, excerpt, frontmatter, slug }) => (
+            <Box
+              key={id}
+              as="article"
+              sx={{
+                mb: 4,
+                p: 3,
+                boxShadow: "0 10px 15px -3px rgba(0, 0, 0, 0.1)",
+                border: "1px solid #d1d1d1",
+                borderRadius: "15px",
+              }}
+            >
+              <Link as={GatsbyLink} to={`/${slug}`}>
+                <Heading>{frontmatter.title}</Heading>
+              </Link>
+            </Box>
+          ))}
+      </Layout>
+    </>
+  );
+}
+
+export const query = graphql`
+  query SITE_INDEX_QUERY {
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        id
+        excerpt(pruneLength: 250)
+        frontmatter {
+          title
+          date(formatString: "YYYY MMMM Do")
+        }
+        slug
+      }
+    }
+  }
+`;
